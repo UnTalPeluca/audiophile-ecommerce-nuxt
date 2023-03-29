7a6e0a98f473f8f8e42c0401cb2567076171c8b9
Rodrigo Vergara <67773102+UnTalPeluca@users.noreply.github.com>
2023-03-29 15:11:08 -0300

    Update NavBar.vue

--- components/header/NavBar.vue
+++ components/header/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <BlackOverlay @click.self="handler(false)" />
-  <nav class="pb-8 px-6 bg-white rounded-b-lg pt-20 absolute top-full left-2 w-full">
+  <nav class="pb-8 px-6 bg-white rounded-b-lg pt-20 absolute top-full left-0 w-full">
     <CategoryList :categories="categories" />
   </nav>
 </template>
